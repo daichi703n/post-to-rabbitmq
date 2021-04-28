ce359b2a509dccc5acf9bbbfe250bcfd66b568ae
Daichi Kimura <daichi703n@gmail.com>
2021-04-29 01:19:19 +0900

    Initial Commit

---
+++ src/main/java/com/daichi703n/posttorabbitmq/PostToRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.daichi703n.posttorabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostToRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostToRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daichi703n/posttorabbitmq/PostToRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daichi703n.posttorabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostToRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
