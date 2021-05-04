3b0cc911af7368d915aaf7d6da2f0cf7a48a38e1
Daichi Kimura <daichi703n@gmail.com>
2021-05-04 23:48:26 +0900

    Add Confirm

--- src/main/java/com/daichi703n/posttorabbitmq/RabbitPublisher.java
+++ src/main/java/com/daichi703n/posttorabbitmq/RabbitPublisher.java
@@ -1,7 +1,11 @@
 package com.daichi703n.posttorabbitmq;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.connection.CorrelationData.Confirm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -19,11 +23,28 @@ public class RabbitPublisher{
     this.rabbitTemplate = rabbitTemplate;
   }
 
+  @Value("${app.is-confirm-enabled:false}")
+  private boolean isConfirmEnabled;
+
   public void publish(String postMessage) throws Exception {
     System.out.println("Sending <"+postMessage+">");
-    rabbitTemplate.convertAndSend(
-      exchange.getName(), "foo.bar.baz", postMessage
+    CorrelationData correlationData = new CorrelationData("Correlation for message "+postMessage);
+
+    if (isConfirmEnabled){
+      System.out.println(correlationData);
+      rabbitTemplate.convertAndSend(
+        exchange.getName(), "foo.bar.baz", postMessage
+        ,correlationData
       );
+      System.out.println("Wait confirm...");
+      Confirm confirm = correlationData.getFuture().get(10, TimeUnit.SECONDS);
+      System.out.println("Confirm received, ack = " + confirm.isAck());
+  } else {
+      rabbitTemplate.convertAndSend(
+        exchange.getName(), "foo.bar.baz", postMessage
+      );
+    }
+  }
 
   }
 }
